d721301a6639dafbe4f233e423371d5615a3fcec
qiwei0727 <qiwei0727@163.com>
2017-07-11 23:56:01 +0800

    添加计时

--- src/com/example/invest/ui/fragment/InvestFragment.java
+++ src/com/example/invest/ui/fragment/InvestFragment.java
@@ -1,5 +1,8 @@
 package com.example.invest.ui.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -7,6 +10,8 @@ import android.widget.Button;
 
 import com.example.invest.R;
 import com.example.invest.common.BaseFragment;
+import com.example.invest.photo.view.PhotoViewActivity;
+import com.example.invest.photo.view.photoView.PhotoView;
 import com.example.invest.ui.activity.MapActivity;
 import com.example.invest.utils.NetUtil;
 
@@ -23,8 +28,14 @@ public class InvestFragment extends BaseFragment implements OnClickListener{
 		btnCheck.setOnClickListener(this);
 		Button btnMap = (Button) rootView.findViewById(R.id.btn_map);
 		btnMap.setOnClickListener(this);
+		rootView.findViewById(R.id.btn_photo_view).setOnClickListener(this);
 	}
 
+    private String[] items = {"http://img3.imgtn.bdimg.com/it/u=3355235453,2337163232&fm=21&gp=0.jpg",
+            "http://img5.imgtn.bdimg.com/it/u=549778725,2120265236&fm=21&gp=0.jpg",
+            "http://img3.imgtn.bdimg.com/it/u=3355235453,2337163232&fm=21&gp=0.jpg",
+            "http://img5.imgtn.bdimg.com/it/u=549778725,2120265236&fm=21&gp=0.jpg"};
+	
 	@Override
 	public void onClick(View arg0) {
 
@@ -37,6 +48,21 @@ public class InvestFragment extends BaseFragment implements OnClickListener{
 			break;
 		case R.id.btn_map:
 			startActivity(new Intent(mActivity, MapActivity.class));
+			break;
+		case R.id.btn_photo_view:
+			 // 1. new Intent
+            Intent intent = new Intent(mActivity, PhotoViewActivity.class);
+            List<String> bannersImages = new ArrayList<String>();
+            for (int i = 0; i < items.length; i++) {
+				bannersImages.add(items[i]);
+			}
+
+            // 2. 传递参数
+            // 2.1 传当前位置
+            intent.putExtra(PhotoViewActivity.CURRENT_POINT_KEY, 0);
+            // 2.2 传总的图片路径  ArrayList
+            intent.putStringArrayListExtra(PhotoViewActivity.IMAGE_URL_LIST_KEY, (ArrayList<String>) bannersImages);
+			startActivity(intent);
 			break;
 		default:
 			break;
--- src/com/example/invest/ui/fragment/MeFragment.java
+++ src/com/example/invest/ui/fragment/MeFragment.java
@@ -1,13 +1,132 @@
 package com.example.invest.ui.fragment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.example.invest.R;
 import com.example.invest.common.BaseFragment;
 
-public class MeFragment extends BaseFragment {
+public class MeFragment extends BaseFragment implements OnClickListener {
+
+	private TextView chronometer, date;
+
+	private Button btnStart, btnStop;
+
+	private long between;
+
+	private Timer timer;
 
 	@Override
 	public int getLayoutID() {
 		return R.layout.fragment_me;
+	}
+
+	@Override
+	public void initView(View rootView) {
+		super.initView(rootView);
+		chronometer = (TextView) rootView.findViewById(R.id.time);
+		date = (TextView) rootView.findViewById(R.id.date);
+		btnStart = (Button) rootView.findViewById(R.id.start);
+		btnStop = (Button) rootView.findViewById(R.id.stop);
+
+		timer = new Timer();
+		btnStart.setOnClickListener(this);
+		btnStop.setOnClickListener(this);
+		time();
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		switch (arg0.getId()) {
+		case R.id.start:
+			TimerTask task = new TimerTask() {
+				@Override
+				public void run() {
+					mActivity.runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							between++;
+							// long day1 = between / (24 * 3600);
+							// long hour1 = between % (24 * 3600) / 3600;
+							long minute1 = between % 3600 / 60;
+							long second1 = between % 60;
+							// System.out.println("" + day1 + "天" + hour1 + "小时"
+							// + minute1 + "分" + second1 + "秒");
+							chronometer
+									.setText(minute1
+											+ ":"
+											+ (String.valueOf(second1).length() == 1 ? "0"
+													+ second1
+													: second1));
+						}
+					});
+				}
+			};
+			timer.schedule(task, 0, 1000); // 延时1000ms后执行，1000ms执行一次
+			break;
+		case R.id.stop:
+			timer.cancel(); // 退出计时器
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	// 当前时间和某个时间点的时间差
+	long time = 0;
+
+	/**
+	 * 距离某个时间点的多长时间
+	 */
+	public void time() {
+		SimpleDateFormat dfs = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		java.util.Date begin = null;
+		try {
+			begin = dfs.parse("2017-07-11 23:07:24");
+
+			java.util.Date end = new Date();// 当前时间
+			time = (end.getTime() - begin.getTime()) / 1000;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				mActivity.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						time++;
+						long minute1 = time % 3600 / 60;
+						long second1 = time % 60;
+						date.setText("已等待"
+								+ minute1
+								+ "分"
+								+ (String.valueOf(second1).length() == 1 ? "0"
+										+ second1 : second1) + "秒");
+					}
+				});
+			}
+		};
+		timer.schedule(task, 0, 1000); // 延时1000ms后执行，1000ms执行一次
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (timer != null) {
+			timer.cancel();
+		}
 	}
 
 }
